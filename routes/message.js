0b2bcfa1ba986f2803f5b1fd54aa46e66a183c28
limheonjun <jumong40@naver.com>
2019-05-07 21:51:12 +0900

    _MMS발송

---
+++ routes/message.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const config = require('../config.js');
+const accountSid = config.accountSid;
+const authToken = config.authToken;
+const client = require('twilio')(accountSid, authToken);
+
+router.get('/', function(req, res){
+  client.messages
+    .create({
+       body: '스마트 화재 예방 서비스 MMS 테스트',
+       from: config.Twilio_NUMBER,
+       mediaUrl: 'https://watermark.lovepik.com/photo/10020/0467.jpg_wh1200.jpg',
+       to: config.My_NUMBER
+     })
+    .then(message => console.log(message.sid));
+});
+
+module.exports = router;
