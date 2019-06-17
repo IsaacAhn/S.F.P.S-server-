f861de7f7557c86d68f8923981aae1c213a7c7ce
dongyub-lee <dongyub010@naver.com>
2019-06-17 10:02:51 +0900

    _mms 수정

--- routes/mms.js
+++ routes/mms.js
@@ -1,19 +1,27 @@
-const express = require('express');
-const router = express.Router();
+module.exports = {
+  send : send
+};
+
 const config = require('../config.js');
 const accountSid = config.accountSid;
 const authToken = config.authToken;
-const client = require('twilio')(accountSid, authToken);
+const client = require('twilio')(accountSid, authToken); // twilio 모듈 로드
 
-router.get('/', function(req, res){
+
+function send(){
   client.messages
     .create({
        body: '스마트 화재 예방 서비스 MMS 테스트',
        from: config.Twilio_NUMBER,
-       mediaUrl: config.Address + 'externalFile.jpg',
+      // mediaUrl: config.Address + 'externalFile.jpg',
        to: config.My_NUMBER
-     })
-    .then(message => console.log(message.sid));
-});
-
-module.exports = router;
+     }, function(error, message){ //콜백 함수 설정
+       if(!error){
+         //console.log(message.sid);
+         console.log(message.dateCreated);
+       } else {
+         console.log(error); //에러발생로그
+       }
+     }
+   )
+}
